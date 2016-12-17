4fa46e5969b2b0b7a2acaa37dac58adc74d1e5a3
Richard Cheng <richardcheng@Richards-MBP.fios-router.home>
2016-12-16 23:07:05 -0800

    Refactor existing code for clarity. Add ability to create new board based on width. Add determine-winner function. Incomplete but resulted in creation of winning-rows function that lists all possible win row combinations.

--- spec/tic_tac_toe/board_spec.clj
+++ spec/tic_tac_toe/board_spec.clj
@@ -2,17 +2,45 @@
   (:require [speclj.core :refer :all]
             [tic-tac-toe.board :refer :all]))
 
-(def empty-board [" " " " " " " " " " " " " " " " " "])
+(def empty-board-3x3 [" " " " " " " " " " " " " " " " " "])
+(def empty-board-4x4 [" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "])
 (def X "X")
 (def O "O")
 (def test-board [X " " " " " " " " " " " " " " " "])
+(def X-first_row-win-board [X X X " " " " " " " " " " " "])
+(def O-second-row-win-board [" " " " " " O O O " " " " " "])
 
-(describe "create board array"
-  (it "should create a board array with size of 9"
-    (should= empty-board (init))))
+(describe "size of board"
+  (it "should figure out size of board given width"
+    (should= 9 (get-size 3))
+    (should= 16 (get-size 4))))
 
-(describe "place-marker"
+(describe "create empty board"
+  (it "should create a board with size of 9 if width is 3"
+    (should= empty-board-3x3 (get-new-board 3)))
+  (it "should create a board with size of 16 if width is 4"
+    (should= empty-board-4x4 (get-new-board 4))))
+
+(describe "place marker"
   (it "should place a marker"
-    (should= [X " " " " " " " " " " " " " " " "] (place-marker empty-board 0 X)))
+    (should= [X " " " " " " " " " " " " " " " "] (place-marker empty-board-3x3 0 X)))
   (it "should not place marker if position is not empty"
     (should= nil (place-marker test-board 0 O))))
+
+(describe "winning row"
+  (it "should return list of winning row combinations if width is 3"
+    (should= `((0 1 2) (3 4 5) (6 7 8)) (winning-rows 3)))
+  (it "should return list of winning row combinatiosn if width is 4"
+    (should= `((0 1 2 3) (4 5 6 7) (8 9 10 11) (12 13 14 15)) (winning-rows 4))))
+
+(describe "winning column"
+  (it "should return list of winning column combinations if width is 3"
+    (should= `((0 3 6) (1 4 7) (2 5 8)) (winning-cols 3)))
+  (it "should return list of winning column combinations if width is 4"
+    (should= `((0 4 8 12) (1 5 9 13) (2 6 10 14) (3 7 11 15)) (winning-cols 4))))
+
+(describe "determine winner"
+	(it "should return winner marker if player won"
+		(should= true (determine-winner X-first_row-win-board X)))
+	(it "should return winner marker if player won"
+		(should= true (determine-winner O-second-row-win-board O))))
--- src/tic_tac_toe/board.clj
+++ src/tic_tac_toe/board.clj
@@ -2,10 +2,23 @@
 
 (def space " ")
 
-(defn init []
-  vector (replicate 9 space))
+(defn get-size [width]
+	(* width width))
+
+(defn get-new-board [width]
+  vector (replicate (get-size width) space))
 
 (defn place-marker [board position marker]
   (if (= space (get board position))
     (assoc board position marker)
     nil))
+
+(defn winning-rows [width]
+	(partition width (range (get-size width))))
+
+(defn winning-cols [width]
+	(for [x (range 0 width)]
+		(range x (get-size width) width)))
+
+(defn determine-winner [board marker]
+	true)
